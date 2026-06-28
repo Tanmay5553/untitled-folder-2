80e3826274458aece33467203c1023d6d8b456e6
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-06-28 11:09:23 +0530

    first progrram

---
+++ FIRST.py
@@ -0,0 +1,4 @@
+print("addition of 90 and 37 is : ,90+37")
+print("substraction of 76 and 24 is : ,76-24")
+print("multiplication of 39 and 51 is : ,39*51")
+print("division of 600 and 35 is : ,600/35") 
