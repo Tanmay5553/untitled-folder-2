9fe55c33b5233a3d67b887e239302a7a41f3123f
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-07-05 11:30:13 +0530

    datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+num=36
+print("type of num is",type(num))
+
+decimal=99.99
+print("type of decimal is",type(decimal))
+
+b=False
+print("type of b is",type(b))
+
+str="hey"
+print("type of str is",type(str))
