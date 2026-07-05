8e0693edac99c72a94353a70d21bf07decc622a5
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-07-05 11:58:17 +0530

    typecasting

---
+++ typrcasting.py
@@ -0,0 +1,24 @@
+name="Tanmay"
+age=14
+height=4.6
+is_student=True
+
+print("name ",name)
+print("data type of name is ",type(name))
+
+print("age ",age)
+print("data type of age is ",type(age))
+
+
+print("height ",height)
+print("data type of height is ",type(height))
+
+print("is student", is_student)
+print("data type of is student is ",type( is_student))
+
+print("type casting ....")
+age=str(age)
+print("data type of age after type casting",type(age))
+
+height=int(height)
+print("data type of height after type casting",type(height))
