f88e54b8f62621e635a4854028b7ac0cb8c0b2ab
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-07-19 09:48:11 +0530

    hw

---
+++ summertimes.py
@@ -0,0 +1,7 @@
+time_am =int(input("enter the time in am :",time_am))
+time_pm =int(input("enter time in pm :",time_pm))
+
+if time_am>time_pm :
+    print("Tanmay has wake up early in the morning"/n",for cycling in holidays")
+else:
+    print("Tanmay has woken up late in the morning"/n",He goes for swimming in holidays")
