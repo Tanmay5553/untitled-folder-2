60b75f239d33177f15351beedf04f3bb39fe7e96
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-07-19 11:44:22 +0530

    and or operations

---
+++ andOr.py
@@ -0,0 +1,13 @@
+num1=59
+num2=91
+num3=0
+
+if num1 and num2 and num3 :
+    print("all the numbers have boolean form true")
+else:
+    print("either of the numbers have boolean form false")
+
+if num1 or num2 or num3 :
+    print("either of the number is true")
+else:
+    print("either of number is false")
