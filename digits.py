1221a74b0ae1820393de4e1b5baa766e5eb88833
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-08-02 12:05:22 +0530

    digits

---
+++ digits.py
@@ -0,0 +1,20 @@
+num=int(input("enter the number :"))
+
+sum=0
+count=0
+
+while count>0:
+
+     count+=1
+     temp//=10
+
+     if sum==num:
+          print("the number is of  ",count,"digits")
+
+else:
+        print("the number is not of ",count,"digits")
+
+
+
+
+    
