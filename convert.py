19d027ad657c4377ed8d9df51b9ec6accb6f41e9
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-08-16 11:49:42 +0530

    convert

---
+++ convert.py
@@ -0,0 +1,12 @@
+decimal ="int",(input("Enter a decimal number: "))
+
+
+binary=""
+
+while decimal > 0:
+ 
+    remainder=decimal%2
+    binary="str",(remainder)+binary
+    decimal//=2
+
+print(" binary :", binary)
