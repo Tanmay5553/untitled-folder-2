dbe61012254ec9bfc97799ecb5808f1a4d2bef24
Tanmay5553 <chetan.girgaonkar@gmail.com>
2026-08-23 12:49:48 +0530

    circles

---
+++ circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+def circumference(radius):
+	return 2 * math.pi * radius
+
+def area(radius):
+	return math.pi * radius ** 2
+
+radius = float(input("Enter the radius: "))
+print(f"Circumference: {circumference(radius)}")
+print(f"Area: {area(radius)}")
